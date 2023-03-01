7613dd06968206fa8a96c927582b33e9c3dd33f3
Bekkhan <87779571856b@gmail.com>
2023-03-01 15:08:07 +0600

    fdg

--- core/tests.py
+++ core/tests.py
@@ -21,27 +21,27 @@ result = requests.post('https://api.survey-studio.com/projects/counters', data=j
 print(result)
 response = result.json()
 print(response['body'][0]['counters']['name'=='Ucell']['value'])
-# ucell_all = 0
-# beeline_all = 0
-# mobiuz_all = 0
-# uzmobile_all = 0
-# for i in range(2, 29):
-#     if i % 2 == 0:
-#         ucell_all += response['body'][0]['counters'][i]['value']
-# for i in range(31, 58):
-#     if i % 2 != 0:
-#         # print(response['body'][i]['quota'])
-#         beeline_all += response['body'][0]['counters'][i]['value']
-# for i in range(60, 87):
-#     if i % 2 == 0:
-#         mobiuz_all += response['body'][0]['counters'][i]['value']
-# for i in range(89, 116):
-#     if i % 2 != 0:
-#         uzmobile_all += response['body'][0]['counters'][i]['value']
-# context = {
-#     'ucell_all': ucell_all,
-#     'beeline_all': beeline_all,
-#     'mobiuz_all': mobiuz_all,
-#     'uzmobile_all': uzmobile_all
-# }
-# print(context)
+ucell_all = 0
+beeline_all = 0
+mobiuz_all = 0
+uzmobile_all = 0
+for i in range(2, 29):
+    if i % 2 == 0:
+        ucell_all += response['body'][0]['counters'][i]['value']
+for i in range(31, 58):
+    if i % 2 != 0:
+        # print(response['body'][i]['quota'])
+        beeline_all += response['body'][0]['counters'][i]['value']
+for i in range(60, 87):
+    if i % 2 == 0:
+        mobiuz_all += response['body'][0]['counters'][i]['value']
+for i in range(89, 116):
+    if i % 2 != 0:
+        uzmobile_all += response['body'][0]['counters'][i]['value']
+context = {
+    'ucell_all': ucell_all,
+    'beeline_all': beeline_all,
+    'mobiuz_all': mobiuz_all,
+    'uzmobile_all': uzmobile_all
+}
+print(context)
--- core/views.py
+++ core/views.py
@@ -245,37 +245,35 @@ def add_to_cart1(request):
 
 
 def dashboards(request):
-    # header = {
-    #     'SS-Token': 'b859f200c0ed4ba491f9a4185f6fb64f',
-    #     'Content-Type': 'application/json-patch+json',
-    #     'accept': 'application/json'
-    # }
-    # payload = {
-    #     "projectIds": [
-    #         '27316'
-    #     ],
-    #     "includeHidden": True
-    # }
-    # result = requests.post('https://api.survey-studio.com/projects/counters', data=json.dumps(payload), headers=header)
-    # # print(result)
-    # response = result.json()
+    header = {
+        'SS-Token': 'b859f200c0ed4ba491f9a4185f6fb64f',
+        'Content-Type': 'application/json-patch+json',
+        'accept': 'application/json'
+    }
+    payload = {
+        "projectIds": [
+            '27316'
+        ],
+        "includeHidden": True
+    }
+    result = requests.post('https://api.survey-studio.com/projects/counters', data=json.dumps(payload), headers=header)
+    response = result.json()
     ucell_all = 0
     beeline_all = 0
     mobiuz_all = 0
     uzmobile_all = 0
-    # for i in range(2, 29):
-    #     if i % 2 == 0:
-    #         ucell_all += response['body'][0]['counters'][i]['value']
-    # for i in range(31, 58):
-    #     if i % 2 != 0:
-    #         # print(response['body'][i]['quota'])
-    #         beeline_all += response['body'][0]['counters'][i]['value']
-    # for i in range(60, 87):
-    #     if i % 2 == 0:
-    #         mobiuz_all += response['body'][0]['counters'][i]['value']
-    # for i in range(89, 116):
-    #     if i % 2 != 0:
-    #         uzmobile_all += response['body'][0]['counters'][i]['value']
+    for i in range(2, 29):
+        if i % 2 == 0:
+            ucell_all += response['body'][0]['counters'][i]['value']
+    for i in range(31, 58):
+        if i % 2 != 0:
+            beeline_all += response['body'][0]['counters'][i]['value']
+    for i in range(60, 87):
+        if i % 2 == 0:
+            mobiuz_all += response['body'][0]['counters'][i]['value']
+    for i in range(89, 116):
+        if i % 2 != 0:
+            uzmobile_all += response['body'][0]['counters'][i]['value']
     context = {
         'ucell_all': ucell_all,
         'beeline_all': beeline_all,
