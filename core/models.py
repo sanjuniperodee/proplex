8c4bff9c09994fc2f07b13c7975310c9632120f9
Bekkhan <87779571856b@gmail.com>
2023-03-07 13:52:30 +0600

    sdf

--- core/models.py
+++ core/models.py
@@ -97,9 +97,6 @@ class Item(models.Model):
             order.items.add(order_item)
             messages.info(request, "This item was added to your cart.")
         return redirect("core:order-summary")
-        return reverse("core:add-to-cart", kwargs={
-            'slug': self.slug
-        })
 
     def get_remove_from_cart_url(self):
         return reverse("core:remove-from-cart", kwargs={
